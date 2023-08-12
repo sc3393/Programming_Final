c2f9daa329455a67734ac5ad5d1a91642bc26e2c
Mona Lisa <138533327+sc3393@users.noreply.github.com>
2023-08-12 14:57:18 -0400

    Added testing

--- App/base_code.py
+++ App/base_code.py
@@ -1,23 +1,31 @@
-ev_database=[
-    {"MSRP": 60, "TYPE":"compact", "STYLE":"innovative", "MAKE":"Hyundai", "MODEL":"Lyric"},
-    {"MSRP": 90, "TYPE":"SUV", "STYLE":"conventional","MAKE":"Ford","MODEL":"F150"},
-    {"MSRP": 120, "TYPE":"sedan", "STYLE":"innovative","MAKE":"Porsche","MODEL":"Taycan"}
-             ]
 
-user_price_min=int(input("Price Min"))
-user_price_max=int(input("Price Max"))
-user_type=input("Type")
-user_style=input("Style")
+def format_usd(my_price):
+    return f"${my_price:,.2f}"
 
-user_picks=[]
+if __name__ =="__main__":
 
-for x in ev_database:
-    if user_price_min <= x["MSRP"] <= user_price_max and (x["TYPE"]== user_type) and (x["STYLE"]== user_style):
-        user_picks.append(x)
-
-if user_picks:
-    print("selection")
-    print(user_picks)
-else:
-    print("none")
+    ev_database=[
+        {"MSRP": 60, "TYPE":"compact", "STYLE":"innovative", "MAKE":"Hyundai", "MODEL":"Lyric"},
+        {"MSRP": 90, "TYPE":"SUV", "STYLE":"conventional","MAKE":"Ford","MODEL":"F150"},
+        {"MSRP": 120, "TYPE":"sedan", "STYLE":"innovative","MAKE":"Porsche","MODEL":"Taycan"}
+                 ]
+    
+    user_price_min=int(input("Price Min"))
+    user_price_max=int(input("Price Max"))
+    user_type=input("Type")
+    user_style=input("Style")
+    
+    user_picks=[]
+    
+    print(format_usd(60))
+    
+    for x in ev_database:
+        if user_price_min <= x["MSRP"] <= user_price_max and (x["TYPE"]== user_type) and (x["STYLE"]== user_style):
+            user_picks.append(x)
+    
+    if user_picks:
+        print("selection")
+        print(user_picks)
+    else:
+        print("none")
     
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Programming_Final
- 
+
+#Create a virtual environment:
+
+```sh
+conda create -n final-env python
+
+```sh
+conda activate final-env
+
+
+
+Install third-party packages:
+
+```sh
+pip install -r requirements.txt
+```
+#Run program
+
+```sh
+python app/base_code.py
+
+export FLASK_APP=web_app flask run
