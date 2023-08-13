12891723e88508926b01a836780ddd3a36b35aca
Abboudaqa <138733150+Abboudaqa@users.noreply.github.com>
2023-08-13 15:09:40 -0400

    commit

--- app/basecode.py
+++ app/basecode.py
@@ -44,6 +44,52 @@ if __name__ =="__main__":
     user_type=input("Type: ").lower()
     user_style=input("Style: ").lower()
     
+    # Import module
+    from tkinter import *
+  
+    # Create object
+    root = Tk()
+  
+    # Adjust size
+    root.geometry( "200x200" )
+  
+    # Change the label text
+    def show():
+    label.config( text = clicked.get() )
+  
+    # Dropdown menu options
+    options = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
+  
+    # datatype of menu text
+    clicked = StringVar()
+  
+    # initial menu text
+    clicked.set( "Monday" )
+  
+    # Create Dropdown menu
+    drop = OptionMenu( root , clicked , *options )
+    drop.pack()
+  
+    # Create button, it will change label text
+    button = Button( root , text = "click Me" , command = show ).pack()
+  
+    # Create Label
+    label = Label( root , text = " " )
+    label.pack()
+  
+    # Execute tkinter
+    root.mainloop()
+
+
+
     user_picks=[]
     
     for x in ev_database:
@@ -51,9 +97,9 @@ if __name__ =="__main__":
             user_picks.append(x)
     
     if user_picks:
-        print("selection")
+        print("Available Options: ")
         for x in user_picks:
             print(f"MSRP: {format_usd(x['MSRP'])} | Type: {x['TYPE']} | Style: {x['STYLE']} | Make: {x['MAKE']} | Model: {x['MODEL']}")
     else:
-        print("none")
+        print("No Available Option for Your Selected criterions, Please Try Again!")
     
